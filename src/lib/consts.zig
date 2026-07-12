0f9d45c3afff529cfbdcf226a2008b593cc3d28b
kubashh <jakubhanula0@gmail.com>
2026-07-12 20:52:38 +0200

    0.1.0 release

--- src/lib/consts.zig
+++ src/lib/consts.zig
@@ -55,7 +55,7 @@ pub const Config = struct {
 
 pub const tmp_alloc = std.heap.page_allocator;
 
-pub const runeVersion = "0.1.0-dev";
+pub const runeVersion = "0.1.0";
 
 //  OsTags
 //      1.  linux, macos, windows
